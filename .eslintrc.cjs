4c7df774a15671f700ead95b0caa0bcd9842c0e0
Vladislav <kovleshenko99@mail.ru>
2024-03-18 09:36:58 +0400

    Clean up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,3 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
@@ -19,8 +13,6 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-
-  // Base config
   extends: ["eslint:recommended"],
 
   overrides: [
