bb79b0843ecebfcf995c2ebecea95258ff776e0a
sergeychama24 <sergeychama24@gmail.com>
2024-03-28 15:19:22 +0300

    add types

--- app/routes/anime.$animeId/route.tsx
+++ app/routes/anime.$animeId/route.tsx
@@ -4,19 +4,26 @@ import { getAnime } from "../anime/anime.server";
 import Button from "@/ui/button/button";
 import {clearHTML} from "@/utils/utils";
 import star from "@/assets/icons/star.svg";
+import {TAnime, TAnimeInfo, TAnimeScreenshots} from "@/types/api/shiki/TAnime";
 
 
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-    const data = {}
+    const data: {
+        rawData: TAnime;
+        screenshots: string[];
+        imageUrl: string;
+        videos: string
+        info: TAnimeInfo[]
+    } = {};
 
     data.rawData = await getAnime(params.animeId);
     data.rawData.description_html = clearHTML(data.rawData.description_html)
-    const screenShots = await getAnime(`${params.animeId}/screenshots`);
-    data.screenshots = screenShots.slice(0, 4).map((item) => `${import.meta.env.VITE_SHIKI_URL}${item.original}`);
+    const screenShots : TAnimeScreenshots[] = await getAnime(`${params.animeId}/screenshots`);
+    data.screenshots = screenShots.slice(0, 4).map((item : TAnimeScreenshots) => `${import.meta["env"]["VITE_SHIKI_URL"]}${item.original}`);
     const rawVideos = await getAnime(`${params.animeId}/videos`);
-    data.videos = rawVideos.slice(0, 2).map(item => item.player_url)
-    data.imageUrl = `${import.meta.env.VITE_SHIKI_URL}${data.rawData.image?.original}`;
+    data.videos = rawVideos.slice(0, 1).map((item: { player_url: string; }) => item.player_url)
+    data.imageUrl = `${import.meta.env["VITE_SHIKI_URL"]}${data.rawData.image?.original}`;
     data.info = [
         {
             title: "тип",
@@ -36,7 +43,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
         },
         {
             title: 'жанры',
-            value: data.rawData.genres.reduce((acc, el) =>  acc + el.russian + ' ', '')
+            value: data.rawData.genres.reduce((acc : string, el : {russian: string}) =>  acc + el.russian + ' ', '')
         },
         {
             title: 'рейтинг',
@@ -44,8 +51,8 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
         },
         {
             title: 'студия',
-            value: data.rawData.studios.reduce((acc, el) =>  acc + el.name + ' ', '')
-        }
+            value: data.rawData.studios.reduce((acc : string, el : {name: string}) =>  acc + el.name + ' ', '')
+        },
     ]
 
     return json(data);
@@ -53,7 +60,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
 export const meta: MetaFunction<typeof loader> = ({data}) => {
     return [
-        { title: `${data.rawData.russian} - Аниме` },
+        { title: `${data ? data.rawData.russian : "Anime page"} - Аниме` },
         { name: 'description', content: 'The best anime project' }
     ]
 }
@@ -62,8 +69,9 @@ export default function AnimePage() {
     const anime = useLoaderData<typeof loader>();
     const scrollToVideo = () => {
         const player = document.getElementById("player");
-        player.scrollIntoView({behavior: "smooth"});
+        player.scrollIntoView({behavior: "smooth"})
     }
+
 
     return (
         anime &&
@@ -87,21 +95,21 @@ export default function AnimePage() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col justify-between col-start-3 col-end-10 px-l">
+            <div className="flex flex-col col-start-3 col-end-10 px-l">
                 <div>
                     <div className="flex flex-wrap items-start justify-between">
                         <h1 className="w-5/6 text-2xl font-bold">{anime.rawData.russian}</h1>
-                        <div className="flex h-fit items-center mt-xs">
+                        <div className="flex h-fit items-center">
                             <img className="w-xl h-xl" src={star} alt=""/>
                             <span className="text-2xl font-bold ml-s">{anime.rawData.score}</span>
                         </div>
                     </div>
-                    <h2 className="text-s mt-xs">{anime.rawData.name}</h2>
+                    <h2 className="text-s mt-xs w-5/6">{anime.rawData.name}</h2>
                     <div className="w-1/6 mt-m">
                         <Button text={'Смотреть'} style={{width: "100%"}} size={"medium"} onClick={scrollToVideo}/>
                     </div>
                 </div>
-                <div className="about mt-l w-3/5 text-xs">
+                <div className="about mt-2xl w-3/5 text-xs">
                     <h3 className="text-m font-bold">Информация</h3>
                     <ul className="about-list mt-m flex flex-col gap-s"> {
                          anime.info.map((el, index) => {
@@ -122,7 +130,7 @@ export default function AnimePage() {
             <div className="mt-l col-start-1 col-end-10">
                 <h3 className="font-bold text-m">Кадры</h3>
                 <div className="flex mt-m">{
-                    anime.screenshots.map((item) => (
+                    anime.screenshots.map((item : string) => (
                         <div className="px-s w-2/6 h-[142px]" key={item}>
                             <img className={"w-full h-full"} src={item} alt={`Кадр из ${anime.rawData.russian}`}/>
                         </div>
@@ -131,7 +139,7 @@ export default function AnimePage() {
             </div>
             <div id="player" className="col-start-1 col-end-10 self-end mt-l">
                 <h2 className="font-bold text-m">Трейлер</h2>
-                <iframe className="mt-m" key='12' title={`Трейлер ${anime.info.russian}`} src={anime.videos} width="100%" height="468px" allowFullScreen></iframe>
+                <iframe className="mt-m" key='12' src="//kodik.info/season/84066/0372efad8c745626a261699d3b24400e/720p" width="100%" height="588px" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
         </div>
     )
--- app/types/api/shiki/TAnime.ts
+++ app/types/api/shiki/TAnime.ts
@@ -1,10 +1,33 @@
 export type TAnime = {
-  id: number
-  russian: string
+  id: number,
+  russian: string,
+  name: string,
   description: string,
+  description_html: string,
   image: {
-    original: string
+    original: string,
+  },
+  score: number,
+  url: string,
+  episodes: string,
+  kind: string,
+  aired_on: string,
+  status: string,
+  genres: [
+  ],
+  rating: string,
+  studios: [
+  ],
+}
+
+export type TAnimeInfo =
+  {
+    title: string,
+    value: string
   }
-  url: string
-  episodes: number
+
+export type TAnimeScreenshots =
+{
+  original: string,
+  preview: string,
 }
