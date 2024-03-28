f3310c9d4d997ba340514d89e380f996521d5bab
Vladislav <kovleshenko99@mail.ru>
2024-03-28 22:46:20 +0400

    fixed changeHandler type and cursor

--- app/ui/checkbox/Checkbox.tsx
+++ app/ui/checkbox/Checkbox.tsx
@@ -1,10 +1,19 @@
+import { ChangeEventHandler } from "react"
 import style from "./checkbox.module.scss"
 
-const Checkbox = ({text, id, onChange} : {text : string, id: string, onChange: any}) => {
-
+const Checkbox = ({text, id, onChange} : {
+    text: string
+    id: string
+    onChange: ChangeEventHandler
+}) => {
     return (
         <label htmlFor={id} className={style.label}>
-            <input className={style.checkbox} type="checkbox" id={id} name={id} onChange={onChange}/>
+            <input className={style.checkbox}
+                type="checkbox"
+                id={id}
+                name={id}
+                onChange={onChange}
+            />
             {text}
         </label>
     )
