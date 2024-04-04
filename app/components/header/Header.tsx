7f6c73997e5805fcc686109673f9c2233f3b2e10
Vladislav <kovleshenko99@mail.ru>
2024-04-05 00:49:52 +0400

    added search to header

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -2,36 +2,45 @@ import { Link } from '@remix-run/react';
 
 import Menu, { TLink } from '@/components/menu/Menu';
 import {LogoIcon} from '@/assets/icons';
+import Search from '@/ui/search/Search'
 
 const Header = () => {
   const links: TLink[] = [
     {
       title: 'Аниме',
       route: '/anime'
-    }, {
+    },
+    {
       title: 'Манга',
       route: '/manga'
     }
-  ];
+  ]
 
   return (
     <header className="sticky z-50 top-[0] w-full py-s bg-black-100">
       <div className="container flex m-auto items-center justify-between">
         <div className="flex items-center">
-          <Link to="/" className="-mt-s">
+          <Link
+            to="/"
+            className="-mt-s"
+          >
             <LogoIcon />
           </Link>
-          {links.length > 0 &&
-            <div className='ml-xl'>
+          {links.length > 0 && (
+            <div className="ml-xl">
               <Menu links={links} />
-            </div>}
+            </div>
+          )}
         </div>
-        <div>
-          <Link to="/login"><p className="text-white font-bold hover:text-accent-100">Войти</p></Link>
+        <div className="flex gap-4xl items-center">
+          <Search placeholder="аниме, манга, студия..." />
+          <Link to="/login">
+            <p className="text-white font-bold hover:text-accent-100">Войти</p>
+          </Link>
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
 export default Header;
--- app/ui/search/Search.tsx
+++ app/ui/search/Search.tsx
@@ -1,39 +1,45 @@
 import Input from "@/ui/input/Input";
-import {TFieldSize} from "@/types/ui";
-import {SearchIcon} from "@/assets/icons";
-import styles from "./search.module.scss"
+import { FieldSize, InputType, TFieldSize } from '@/types/ui'
+import { SearchIcon } from '@/assets/icons'
+import styles from './search.module.scss'
 
 type TSearch = {
-    size?: TFieldSize
-    disabled?: boolean
-    placeholder?: string
-    value?: string
-    name?: string
-    onChange?: any
+  size?: TFieldSize
+  disabled?: boolean
+  placeholder?: string
+  value?: string
+  name?: string
+  onChange?: any
 }
 
-const Search = (props:TSearch) => {
-    const {
-        size = 'small',
-        placeholder = 'default placeholder',
-        disabled = false,
-        value,
-        name,
-        onChange
-    } = props
-    return (
-        <div className={styles.search}>
-            <Input size={size}
-                   placeholder={placeholder}
-                   value={value}
-                   name={name}
-                   disabled={disabled}>
+const Search = (props: TSearch) => {
+  const {
+    size = FieldSize.small,
+    placeholder = 'default placeholder',
+    disabled = false,
+    value,
+    name,
+    onChange
+  } = props
 
-            </Input>
-            <SearchIcon className={styles["search__icon"]} onClick={onChange}/>
-        </div>
-
-    )
+  return (
+    <div className={styles.search}>
+      <Input
+        size={size}
+        placeholder={placeholder}
+        value={value}
+        name={name}
+        disabled={disabled}
+        type={InputType.transparent}
+      />
+      <span className="px-s">
+        <SearchIcon
+          className="cursor-pointer w-m min-w-m h-m"
+          onClick={onChange}
+        />
+      </span>
+    </div>
+  )
 }
 
 export default Search;
