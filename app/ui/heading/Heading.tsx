fe764fd834339d91bddd5d9035b1d5213ffbea0c
Sergey Chama <sergeychama24@gmail.com>
2024-08-06 05:44:16 +0300

    added heading component

--- app/ui/heading/Heading.tsx
+++ app/ui/heading/Heading.tsx
@@ -1,20 +1,32 @@
 import { ReactNode } from 'react'
 import styles from './heading.module.scss'
-import { Link } from '@remix-run/react'
 
 type THeadingProps = {
-  children?: ReactNode
+  size: string
+  children: ReactNode
 }
 
 const Heading = (props: THeadingProps) => {
-  return (
-    <Link
-      className={styles.heading__container}
-      to={'/anime'}
-    >
-      <h3 className={styles.heading}>{props.children}</h3>
-    </Link>
-  )
+  switch (props.size) {
+    case 'h1': {
+      return <h1 className={styles.heading}>{props.children}</h1>
+    }
+    case 'h2': {
+      return <h2 className={styles.heading}>{props.children}</h2>
+    }
+    case 'h3': {
+      return <h3 className={styles.heading}>{props.children}</h3>
+    }
+    case 'h4': {
+      return <h4 className={styles.heading}>{props.children}</h4>
+    }
+    case 'h5': {
+      return <h5 className={styles.heading}>{props.children}</h5>
+    }
+    case 'h6': {
+      return <h6 className={styles.heading}>{props.children}</h6>
+    }
+  }
 }
 
 export default Heading
