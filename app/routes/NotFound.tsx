7445b3dc4a9ef5d109657cdacaa56560c383fda7
Sergey Chama <sergeychama24@gmail.com>
2024-08-06 05:38:35 +0300

    rework notFound Page

--- app/routes/NotFound.tsx
+++ app/routes/NotFound.tsx
@@ -1,15 +1,18 @@
 import { Link } from '@remix-run/react'
-import { NotFoundIcon } from '@/assets/icons'
+import { NotFoundDarkIcon, NotFoundLightIcon } from '@/assets/icons'
 import { useTranslation } from 'react-i18next'
+import { useTheme } from '@/context/ThemeContext'
 
 export const handle = { i18n: ['404'] }
 
 export default function NotFound() {
   const { t } = useTranslation('404')
+  const { theme } = useTheme()
+
   return (
     <main className="container py-s sm:py-m md:py-l lg:py-xl text-black-100 min-h-[100vh] flex flex-col justify-center gap-l">
       <div className="mx-auto text-center">
-        <h1 className="text-[48px] mb-m text-black-80 font-bold">
+        <h1 className="text-[48px] mb-m text-black-80 font-bold dark:text-black-40">
           {t('page not found')}
         </h1>
         <h3 className="text-black-60 mb-s">{t('sorry, page not found')}</h3>
@@ -20,7 +23,7 @@ export default function NotFound() {
         </Link>
       </div>
       <div className="w-full md:w-[80%] mx-auto inline-block">
-        <NotFoundIcon />
+        {theme === 'light' ? <NotFoundLightIcon /> : <NotFoundDarkIcon />}
       </div>
     </main>
   )
