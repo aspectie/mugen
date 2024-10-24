49a463347e3c4ffe4eaf95a1535655c9830897a5
Sergey Chama <sergeychama24@gmail.com>
2024-10-25 00:25:56 +0300

    fix button width on small screens and header font-size

--- pages/forgot/ui/ForgotPage.tsx
+++ pages/forgot/ui/ForgotPage.tsx
@@ -14,11 +14,10 @@ export function ForgotPage() {
   return (
     <div className="h-full flex items-center justify-center">
       <section className="flex flex-col p-l m-auto bg-gray-20 w-full sm:w-4/5 lg:w-3/5 xl:w-2/5 rounded-lg shadow-md">
-        <div className="flex justify-between items-end mb-2xl">
-          <h1 className="font-bold text-black-80">{t('recover password')}</h1>
-          <Link to="/register">
-            <h5 className="hover:text-accent-100">{t('registration')}</h5>
-          </Link>
+        <div className="flex justify-between mb-2xl">
+          <h1 className="font-bold text-black-80 text-m">
+            {t('recover password')}
+          </h1>
         </div>
         <ValidatedForm
           validator={validator}
@@ -33,10 +32,18 @@ export function ForgotPage() {
             </FormField>
           </div>
 
-          <div className="w-full flex justify-between">
-            <div className="w-1/3">
+          <div className="w-full flex justify-between content-center">
+            <div className="w-fit">
               <Button text={t('recover')} />
             </div>
+            <Link
+              to="/register"
+              className="py-[1px]"
+            >
+              <h5 className="hover:text-accent-100 text-xs text-center">
+                {t('registration')}
+              </h5>
+            </Link>
           </div>
         </ValidatedForm>
       </section>
